b13a6fe3387385a26d797c9155b3575503b03e80
weavrk <weavrk@gmail.com>
2022-06-23 23:57:00 -0500

    loaded recipe names with for loop

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 
 const button = document.querySelector('button')
 const ingredientInput = document.querySelector('input')
-const imageDiv = document.querySelector('div')
+const imageThumb = document.querySelectorAll('.thumbnail')
+const thumbTitle = document.querySelectorAll('.thumbnail-header')
 
 
 const getRecipe = async () => {
   let ingredient = ingredientInput.value
-  let findRecipe = await axios.get(
-    `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=218e6a8e&app_key=900ce9d6818c4e1417a4bc410a2829b0`
-  )
+  let findRecipe = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=218e6a8e&app_key=900ce9d6818c4e1417a4bc410a2829b0`)
   console.log(findRecipe)
+  let searchResults = findRecipe.data.hits
+  let resultsList = []
+  for (let i = 0; i < searchResults.length; i++) {
+    let recipeThumb = findRecipe.data.hits[i].recipe.images.THUMBNAIL
+    let recipeNames = findRecipe.data.hits[i].recipe.label
+    let recipeLinks = findRecipe.data.hits[i].recipe.url
+    resultsList.push(recipeNames)
+    thumbTitle[i].innerText = resultsList[i]
+    thumbTitle[i].href = recipeLinks
+  }
 }
 
 button.addEventListener('click', getRecipe)
-
 
 
 
