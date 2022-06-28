7a42dcfcb6de5afe164872bba46ab9f54b232f2b
weavrk <weavrk@gmail.com>
2022-06-28 14:43:12 -0500

    hyperlink working

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const button = document.querySelector('button')
 const ingredientInput = document.querySelector('input')
 const imageThumb = document.querySelectorAll('.thumbnail')
 const thumbTitle = document.querySelectorAll('.thumbnail-header')
+const search = document.querySelector('.search')
 
 
 const getRecipe = async () => {
+  let anchor = document.createElement("a");
+  anchor.href = "recipe_info.html";
+  anchor.innerText = "recipe overview";
+  search.appendChild(anchor).classList.add("recipe-overview");
+
   let ingredient = ingredientInput.value
   let findRecipe = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=218e6a8e&app_key=900ce9d6818c4e1417a4bc410a2829b0`)
   console.log(findRecipe)
@@ -18,8 +24,7 @@ const getRecipe = async () => {
     resultsList.push(recipeNames)
     thumbTitle[i].innerText = resultsList[i]
     thumbTitle[i].href = recipeLinks
-    // imageThumb[i].innerHTML = `<img src=${recipeThumb} alt = "recipe"/>`
-    // imageThumb[i].style.backgroundImage = "url(images/img.jpg)"
+    // Alernate if you want to place an image instead... imageThumb[i].innerHTML = `<img src=${recipeThumb} alt = "recipe"/>`
     imageThumb[i].style.backgroundImage = `url(${recipeThumb})`
   }
 }
