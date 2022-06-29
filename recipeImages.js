372efd1a48249d439c24008d080f7270c5d32e4a
weavrk <weavrk@gmail.com>
2022-06-28 22:17:27 -0500

    got the images link to carry the ingredients value from the index.html and display image thumbs

--- recipeImages.js
+++ recipeImages.js
@@ -10,9 +10,32 @@ let recipeSummary = document.querySelector('.recipe-summary')
 let recipeImages = document.querySelector('.recipe-images')
 
 
-//pulls the images based on the search
+//OLD SEARCH
 const getRecipe = async () => {
   let ingredient = ingredientStorage
+  let findRecipe = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=218e6a8e&app_key=900ce9d6818c4e1417a4bc410a2829b0`)
+  console.log(findRecipe)
+  const recipeResultsImages = () => {
+    let searchResults = findRecipe.data.hits
+    let resultsList = []
+    for (let i = 0; i < searchResults.length; i++) {
+      let recipeThumb = findRecipe.data.hits[i].recipe.image
+      let recipeNames = findRecipe.data.hits[i].recipe.label
+      let recipeLinks = findRecipe.data.hits[i].recipe.url
+      resultsList.push(recipeNames)
+      thumbTitle[i].innerText = resultsList[i]
+      thumbTitle[i].href = recipeLinks
+      // Alernate for placing an image instead... imageThumb[i].innerHTML = `<img src=${recipeThumb} alt = "recipe"/>`
+      imageThumb[i].style.backgroundImage = `url(${recipeThumb})`
+    }
+  }
+  recipeResultsImages()
+}
+getRecipe()
+
+//NEW SEARCH
+const getNewRecipe = async () => {
+  let ingredient = ingredientInput.value
   let findRecipe = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=218e6a8e&app_key=900ce9d6818c4e1417a4bc410a2829b0`)
   console.log(findRecipe)
   //THIS MAKES THE DATA GLOBAL BETWEEN PAGES!!
@@ -34,4 +57,4 @@ const getRecipe = async () => {
   recipeResultsImages()
 }
 
-button.addEventListener('click', getRecipe)
+button.addEventListener('click', getNewRecipe)
